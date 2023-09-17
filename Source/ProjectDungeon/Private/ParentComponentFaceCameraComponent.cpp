8e552c64ca022bbf34c5bbb33b7b179219840101
Ta David Yu <ta.yu.gamedev@gmail.com>
2023-09-17 23:50:58 +0200

    Removed stupid widget following logic, and used screen space wdiget component directly

---
+++ Source/ProjectDungeon/Private/ParentComponentFaceCameraComponent.cpp
@@ -0,0 +1,58 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ParentComponentFaceCameraComponent.h"
+
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMathLibrary.h"
+
+
+// Sets default values for this component's properties
+UParentComponentFaceCameraComponent::UParentComponentFaceCameraComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UParentComponentFaceCameraComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UParentComponentFaceCameraComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                               FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+	auto *parent = GetAttachParent();
+	if (parent == nullptr)
+	{
+		return;
+	}
+	
+	APlayerCameraManager const *cameraManager = UGameplayStatics::GetPlayerCameraManager(GetWorld(), TargetCameraPlayerIndex);
+	if (cameraManager == nullptr)
+	{
+		return;
+	}
+	
+	auto const location = parent->GetComponentLocation();
+	auto const cameraLocation = cameraManager->GetCameraLocation();
+
+	FRotator lookRotation = UKismetMathLibrary::FindLookAtRotation(location, cameraLocation);
+	lookRotation.Roll = 0;
+	
+	parent->SetWorldRotation(lookRotation);
+}
+
---
+++ Source/ProjectDungeon/Public/ParentComponentFaceCameraComponent.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SceneComponent.h"
+#include "ParentComponentFaceCameraComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class PROJECTDUNGEON_API UParentComponentFaceCameraComponent : public USceneComponent
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 TargetCameraPlayerIndex = 0;
+	
+public:
+	// Sets default values for this component's properties
+	UParentComponentFaceCameraComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+};
